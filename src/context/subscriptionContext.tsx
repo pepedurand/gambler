7e1d1960b85c743eec01f3c21303590467be971b
Pedro Durand <webpedrod@gmail.com>
2024-05-24 00:10:13 -0300

    refactor: ajuste de carregamento de acesso do user

--- src/context/subscriptionContext.tsx
+++ src/context/subscriptionContext.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { useGetSubscriptionStatus } from "@/hooks/useGetSubscriptionStatus";
 import { createContext, useContext, useEffect, useState } from "react";
 import { useAuth } from "./authContext";
+import { getSubscriptionStatus } from "@/api/subscription";
 
 const SubscriptionContext = createContext({
   userHasAccess: false,
@@ -19,19 +19,24 @@ export function SubscriptionProvider({
 }) {
   const [userHasAccess, setUserHasAccess] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-  const { data: subscriptionStatus, isLoading: isLoadingApi } =
-    useGetSubscriptionStatus();
-  const { isUserLoggedIn } = useAuth();
+  const { currentUser, isUserLoggedIn } = useAuth();
 
   useEffect(() => {
-    if (!isLoadingApi && subscriptionStatus && isUserLoggedIn) {
-      if (subscriptionStatus.status === "active") {
+    async function checkSubscriptionStatus() {
+      const email = currentUser?.email ?? null;
+      const subscriptionStatus = await getSubscriptionStatus(email);
+      if (subscriptionStatus && subscriptionStatus?.status === "active") {
         return setUserHasAccess(true), setIsLoading(false);
       }
-      setUserHasAccess(false);
-      setIsLoading(false);
+      setUserHasAccess(false), setIsLoading(false);
     }
-  }, [isLoadingApi, subscriptionStatus, isUserLoggedIn]);
+    if (isUserLoggedIn) {
+      checkSubscriptionStatus();
+    }
+    if (!isUserLoggedIn) {
+      setUserHasAccess(false);
+    }
+  }, [isUserLoggedIn]);
 
   const value = { userHasAccess, isLoading };
 
